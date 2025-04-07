eac862816bc49a2a9abf9eca4287a6e633d2c72c
weiwei <wutamy77@gmail.com>
2025-04-07 22:11:11 +0800

    create document vectorization and use it in main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
 
 model = OllamaLLM(model="llama3.2")
 
@@ -15,11 +16,13 @@ prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
 while True:
-    print("\n\n------------------------------------------")
+    print("\n------------------------------------------")
     question = input("Ask your question (q to quit): ")
-    print("\n\n------------------------------------------")
+    print("\n------------------------------------------")
 
     if question == "q":
         break
-result = chain.invoke({"reviews":[], "question":question})
-print(result)
+
+    reviews = retriever.invoke(question)
+    result = chain.invoke({"reviews":reviews, "question":question})
+    print(result)
